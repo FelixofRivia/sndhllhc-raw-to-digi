12815349d75cfa957f4a2ebd3e583270b03d56b8
fmei <fmei@cern.ch>
2026-04-14 11:33:06 +0200

    read input file

--- include/FEDRawData.h
+++ include/FEDRawData.h
@@ -1,12 +1,12 @@
-#define SNDHLLHC_RAWTODIGI_FEDRAWDATA
 #ifndef SNDHLLHC_RAWTODIGI_FEDRAWDATA
+#define SNDHLLHC_RAWTODIGI_FEDRAWDATA
 
 namespace sndhllhc {
     class FEDRawData {
         public:
             const unsigned char * data() const { return data_.data(); }
         private:
-            std::vector<unsigned char> data_:
+            std::vector<unsigned char> data_;
     };
 }
 
--- raw_to_digi.cxx
+++ raw_to_digi.cxx
@@ -1,11 +1,26 @@
 #include <iostream>
+#include <vector>
+#include <memory>
+#include <string>
 #include "TFile.h"
+#include "TTree.h"
 
 #include "Wrapper.h"
 #include "FEDRawData.h"
 
 int main(){
-    std::cout << "I like RNTuples\n";
-    TFile *file = new TFile("test.root", "RECREATE");
-    file->Close();
+    std::string input_path = "/home/filippo/raw_to_digi/raw_data/run1000779/run1000779.root";
+    std::unique_ptr<TFile> input_raw_file( TFile::Open(input_path.c_str(), "READ") );
+
+    auto input_raw_tree = input_raw_file->Get<TTree>("Events");
+    input_raw_tree->SetBranchStatus("*", false);
+    input_raw_tree->SetBranchStatus("FEDRawDataCollection_rawDataCollector__LHC.", true);
+
+    sndhllhc::Wrapper<std::vector<sndhllhc::FEDRawData>> raw_data;
+    input_raw_tree->SetBranchAddress("FEDRawDataCollection_rawDataCollector__LHC.", &raw_data);
+
+    for(int i{0}; i < input_raw_tree->GetEntries(); ++i){
+        std::cout << "Entry: " << i << "\n";
+        if (raw_data.product()) std::cout << "present\n";
+    }
 }
