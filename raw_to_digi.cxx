07f7d44791abeff27d4aa039f5a8c8a1711734cb
fmei <fmei@cern.ch>
2026-04-15 14:33:04 +0200

    dump raw data

--- raw_to_digi.cxx
+++ raw_to_digi.cxx
@@ -15,12 +15,12 @@ int main(){
     input_raw_tree->SetBranchStatus("*", false);
     input_raw_tree->SetBranchStatus("FEDRawDataCollection_rawDataCollector__LHC.*", true);
 
-    // input_raw_tree->Scan("FEDRawDataCollection_rawDataCollector__LHC.present");
+    //input_raw_tree->Scan("FEDRawDataCollection_rawDataCollector__LHC.obj.data_.data_");
 
     edm::Wrapper<FEDRawDataCollection> *raw_data{nullptr};
     input_raw_tree->SetBranchAddress("FEDRawDataCollection_rawDataCollector__LHC.", &raw_data);
     
-    for(int i{0}; i < input_raw_tree->GetEntries(); ++i){
+    for(int i{1}; i < input_raw_tree->GetEntries() && i<2; ++i){
         input_raw_tree->GetEntry(i);
         std::cout << "Entry: " << i << "\n";
         if (!raw_data){
@@ -28,7 +28,16 @@ int main(){
             continue;
         }
         std::cout << "raw_data->present: " << raw_data->present << "\n";
-        // std::cout << "raw_data->product(): " << raw_data->product() << "\n";
-        // if (raw_data->present) std::cout << "present\n";
+        std::vector<FEDRawData> feds = raw_data->obj.data_;
+        std::cout << "feds.size(): " << feds.size() << "\n";
+        for (size_t fed_index{0}; fed_index < feds.size(); ++fed_index) {
+            if (feds[fed_index].data_.size() > 0) {
+                size_t raw_size = feds[fed_index].data_.size();
+                std::cout << "fed index: " << fed_index << "\tsize: " << raw_size << "\n";
+                for (size_t raw_index{0}; raw_index < raw_size; ++raw_index) {
+                    printf("raw index: %u data: %hhu \n", raw_index, feds[fed_index].data_[raw_index]);
+                }
+            }
+        }
     }
 }
