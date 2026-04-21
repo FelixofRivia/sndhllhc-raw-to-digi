085c88b89c8c084ca887e67c9c21b7b2601c582a
fmei <fmei@cern.ch>
2026-04-21 17:39:02 +0200

    everything needed

--- raw_to_digi/include/SiStripRawToDigi.h
+++ raw_to_digi/include/SiStripRawToDigi.h
@@ -4,92 +4,13 @@
 #include "SiStripIOHeaders.h"
 #include <vector>
 
-  //holds information about position of a channel in the buffer for use by unpacker
-class FEDChannel {
-    public:
-        enum class ZSROMode { nonLite = 7, lite = 2 };
-        constexpr FEDChannel(const uint8_t* const data, const uint32_t offset, const uint16_t length);
-        constexpr FEDChannel(const uint8_t* const data, const uint32_t offset, const ZSROMode zsRoMode);
-        //gets length from first 2 bytes (assuming normal FED channel)
-        constexpr FEDChannel(const uint8_t* const data, const uint32_t offset);
-        constexpr uint16_t stripsInCh(uint8_t num_bits) const;
-        constexpr uint16_t length() const;
-        constexpr const uint8_t* data() const;
-        constexpr uint32_t offset() const;
-        /**
-            * Retrieve the APV CM median for a non-lite zero-suppressed channel
-            *
-            * apvIndex should be either 0 or 1 (there are, by construction, two APVs on every channel)
-            * No additional checks are done here, so the caller should check
-            * the readout mode and/or packet code.
-            */
-        constexpr uint16_t cmMedian(const uint8_t apvIndex) const;
-        //third byte of channel data for normal FED channels
-        constexpr uint8_t packetCode() const;
-
-    private:
-        friend class FEDBuffer;
-        const uint8_t* data_;
-        uint32_t offset_;
-        uint16_t length_;
-        uint8_t headerLen_ = 0;
-};
-
-constexpr FEDChannel::FEDChannel(const uint8_t* const data, const uint32_t offset, const ZSROMode zsRoMode)
-    : data_(data), offset_(offset), headerLen_(zsRoMode == ZSROMode::nonLite ? 7 : 2) {
-    length_ = (data_[(offset_) ^ 7] + (data_[(offset_ + 1) ^ 7] << 8));
-}
-
-constexpr FEDChannel::FEDChannel(const uint8_t* const data, const uint32_t offset) : data_(data), offset_(offset) {
-    length_ = (data_[(offset_) ^ 7] + (data_[(offset_ + 1) ^ 7] << 8));
-}
-
-constexpr FEDChannel::FEDChannel(const uint8_t* const data, const uint32_t offset, const uint16_t length)
-    : data_(data), offset_(offset), length_(length) {}
-
-constexpr uint16_t FEDChannel::stripsInCh(uint8_t num_bits) const {
-    const bool emptyCh = (headerLen_ + 2) >= (length_);
-    const uint16_t start = offset_ + headerLen_;
-    const uint16_t end = offset_ + length_;
-    uint16_t stripN = 0;
-    if (!emptyCh) {
-        for (uint16_t nStrip_wOfs = start + 1; nStrip_wOfs < end;) {
-        const uint8_t clustStripN = data_[(nStrip_wOfs) ^ 7];
-        nStrip_wOfs += ((uint32_t)clustStripN) * num_bits / 8 + 2;
-        stripN += clustStripN;
-        }
-    }
-    return stripN;
-}
-
-constexpr uint16_t FEDChannel::length() const { return length_; }
-
-constexpr uint8_t FEDChannel::packetCode() const { return data_[(offset_ + 2) ^ 7]; }
-
-constexpr uint16_t FEDChannel::cmMedian(const uint8_t apvIndex) const {
-    uint16_t result = 0;
-    //CM median is 10 bits with lowest order byte first. First APV CM median starts in 4th byte of channel data
-    result |= data_[(offset_ + 3 + 2 * apvIndex) ^ 7];
-    result |= (((data_[(offset_ + 4 + 2 * apvIndex) ^ 7]) << 8) & 0x300);
-    return result;
-}
-
-constexpr const uint8_t* FEDChannel::data() const { return data_; }
-
-constexpr uint32_t FEDChannel::offset() const { return offset_; }
-  
-
-
-
-class SiStripRawToDigi {
-    public:
-        SiStripRawToDigi() = default;
-        std::vector<SiStripDigi> operator()(const edm::Wrapper<FEDRawDataCollection>& sistrip_raw) const;
-};
-
 std::vector<SiStripDigi> SiStripRawToDigi::operator()(const edm::Wrapper<FEDRawDataCollection>& sistrip_raw) const {
     size_t fed_index{116};
     FEDRawData data = sistrip_raw.obj.data[fed_index];
+    FEDBuffer buffer(data);
+    buffer.findChannels();
+    FEDChannel channel = buffer.channel(42);
+    
     std::vector<SiStripDigi> digis;
     SiStripDigi(stripStart + firstStrip + inCluster, getADC_W<num_words>(data, offset, bits_shift);
     for (uint16_t i{0}; i < 4; ++i) {
