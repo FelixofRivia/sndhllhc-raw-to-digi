2b9642dc56f9c6cd9c029e4849d3a673add205d2
fmei <fmei@cern.ch>
2026-04-30 13:41:19 +0200

    more histos

--- raw_to_digi/src/real_time_monitoring.cxx
+++ raw_to_digi/src/real_time_monitoring.cxx
@@ -13,14 +13,11 @@
 int main(int argc, char* argv[]){
     if (argc != 3) {
         std::cerr << "2 arguments expected but " << argc - 1 << " provided\n";
-        std::cerr << "Usage: raw_to_digi <input_path> <output_path>\n";
+        std::cerr << "Usage: raw_to_digi <input_path> <output_directory>\n";
         return 1;
     }
     std::string input_path(argv[1]);
-    std::string output_path(argv[2]);
-
-    auto df = ROOT::RDataFrame("Events", input_path);
-    auto df2 = df.Define("FedChannelDigis", SiStripRawToDigi(), {"FEDRawDataCollection_rawDataCollector__LHC."});
+    std::string output_directory(argv[2]);
 
     auto extract_adc = [](const std::vector<SiStripDigi>& digis) {
         std::vector<uint16_t> adcs;
@@ -31,10 +28,42 @@ int main(int argc, char* argv[]){
         return adcs;
     };
 
-    auto df3 = df2.Define("ADC", extract_adc, {"FedChannelDigis"});
-    auto histo_adc = df3.Histo1D<std::vector<uint16_t>>({"SiStrip adc", "SiStrip adc", 512, 0, 512}, "ADC");
+    auto extract_strip = [](const std::vector<SiStripDigi>& digis) {
+        std::vector<uint16_t> strips;
+        strips.reserve((digis.size()));
+        for (const auto& digi : digis) {
+            strips.push_back(digi.strip());
+        }
+        return strips;
+    };
 
-    TCanvas canvas("canvas", "SiStrip ADC Histogram", 800, 600);
+    auto extract_id = [](const std::vector<SiStripDigi>& digis) {
+        std::vector<uint32_t> ids;
+        ids.reserve((digis.size()));
+        for (const auto& digi : digis) {
+            ids.push_back(digi.id());
+        }
+        return ids;
+    };
+
+    auto df = ROOT::RDataFrame("Events", input_path);
+    auto df2 = df.Define("FedChannelDigis", SiStripRawToDigi(), {"FEDRawDataCollection_rawDataCollector__LHC."});
+    auto df3 = df2.Define("ADC", extract_adc, {"FedChannelDigis"});
+    auto df4 = df3.Define("strip", extract_strip, {"FedChannelDigis"});
+    auto df5 = df4.Define("id", extract_id, {"FedChannelDigis"});
+    auto histo_adc = df5.Histo1D<std::vector<uint16_t>>({"SiStrip adc", "SiStrip adc", 512, 0, 512}, "ADC");
+    auto histo_strip = df5.Histo1D<std::vector<uint16_t>>({"SiStrip strip", "SiStrip strip", 512, 0, 512}, "strip");
+    auto histo_id = df5.Histo1D<std::vector<uint32_t>>({"SiStrip id", "SiStrip id", 512, 0, 512}, "id");
+
+    TCanvas canvas_adc("canvas adc", "SiStrip ADC Histogram", 800, 600);
     histo_adc->Draw();
-    canvas.SaveAs(output_path.c_str());
+    canvas_adc.SaveAs((output_directory + std::string("/sistrip_adc.png")).c_str());
+
+    TCanvas canvas_strip("canvas strip", "SiStrip strip Histogram", 800, 600);
+    histo_strip->Draw();
+    canvas_strip.SaveAs((output_directory + std::string("/sistrip_strip.png")).c_str());
+
+    TCanvas canvas_id("canvas id", "SiStrip id Histogram", 800, 600);
+    histo_id->Draw();
+    canvas_id.SaveAs((output_directory + std::string("/sistrip_id.png")).c_str());
 }
