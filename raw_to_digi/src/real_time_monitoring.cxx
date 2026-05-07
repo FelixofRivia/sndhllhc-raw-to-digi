de0aad039fec2fefa424e2e6b8f378397af4a6af
fmei <fmei@cern.ch>
2026-05-07 12:29:44 +0200

    add option for root7 histos

--- raw_to_digi/src/real_time_monitoring.cxx
+++ raw_to_digi/src/real_time_monitoring.cxx
@@ -7,6 +7,11 @@
 #include "TCanvas.h"
 #include "TH1D.h"
 
+#if USE_ROOT7_RHIST
+#include "ROOT/RHist.hxx"
+#include "ROOT/Hist/ConvertToTH1.hxx"
+#endif
+
 #include "SiStripIOHeaders.h"
 #include "SiStripRawToDigi.h"
 
@@ -53,9 +58,21 @@ int main(int argc, char* argv[]){
     auto df3 = df2.Define("ADC", extract_adc, {"FedChannelDigis"});
     auto df4 = df3.Define("strip", extract_strip, {"FedChannelDigis"});
     auto df5 = df4.Define("id", extract_id, {"FedChannelDigis"});
+
+    //If RHist is available
+    #if USE_ROOT7_RHIST
+    auto rhist_adc = df5.Hist(512, {0, 512}, "ADC");
+    auto rhist_strip = df5.Hist(512, {0, 512}, "strip");
+    auto rhist_id = df5.Hist(512, {0, 512}, "id");
+    auto histo_adc = ROOT::Experimental::Hist::ConvertToTH1D(*rhist_adc);
+    auto histo_strip = ROOT::Experimental::Hist::ConvertToTH1D(*rhist_strip);
+    auto histo_id = ROOT::Experimental::Hist::ConvertToTH1D(*rhist_id);
+    // If RHist is not available
+    #else
     auto histo_adc = df5.Histo1D<std::vector<uint16_t>>({"SiStrip adc", "SiStrip adc", 512, 0, 512}, "ADC");
     auto histo_strip = df5.Histo1D<std::vector<uint16_t>>({"SiStrip strip", "SiStrip strip", 512, 0, 512}, "strip");
     auto histo_id = df5.Histo1D<std::vector<uint32_t>>({"SiStrip id", "SiStrip id", 512, 0, 512}, "id");
+    #endif
 
     TCanvas canvas_adc("canvas adc", "SiStrip ADC Histogram", 800, 600);
     histo_adc->Draw();
