e509196bbd9185609b219570db741b227981c737
FelixofRivia <filippomei99@gmail.com>
2026-05-27 18:49:20 +0200

    test

--- python/libs/run_conversion.py
+++ python/libs/run_conversion.py
@@ -10,7 +10,7 @@ def run_conversion(directories, run_number):
     current_dir = Path.cwd()
     source_cms = "source /cvmfs/cms.cern.ch/cmsset_default.sh"
     cmsenv = f"cd {directories['cmssw_src']} && cmsenv && cd {current_dir}"
-    clean_previous_conversion = f"rm -f {raw_folder / '*index*.jsn'} {raw_folder / 'processing'} {raw_folder / 'open'} {raw_folder / 'mon'} {raw_folder / '*.BoLS.jsn'}"
+    clean_previous_conversion = f"rm -rf {raw_folder / '*index*.jsn'} {raw_folder / 'processing'} {raw_folder / 'open'} {raw_folder / 'mon'} {raw_folder / '*.BoLS.jsn'}"
     reset_progress = f"echo '1 0' > {raw_folder / 'fu.lock'}"
     conversion = f"cmsRun cms_raw_evt_building.py rawDirectory={directories['raw']} convertedDirectory={directories['converted']} runNumber={run_number}"
 
