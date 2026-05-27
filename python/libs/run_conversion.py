3b1c4d67b30ef868eef9ec9127d1bf6a746a4a8f
FelixofRivia <filippomei99@gmail.com>
2026-05-27 18:45:40 +0200

    test

--- python/libs/run_conversion.py
+++ python/libs/run_conversion.py
@@ -9,7 +9,7 @@ def run_conversion(directories, run_number):
     current_dir = Path.cwd()
     source_cms = "source /cvmfs/cms.cern.ch/cmsset_default.sh"
     cmsenv = f"cd {directories['cmssw_src']} && cmsenv && cd {current_dir}"
-    clean_previous_conversion = f"rm -f {(directories['raw'] / f'run{run_number:06d}') / '*index*.jsn'}"
+    clean_previous_conversion = f"rm -f {(directories['raw'] / f'run{run_number:06d}') / '*index*.jsn'} processing open mon *.BoLS.jsn"
     reset_progress = f"echo '1 0' > {(directories['raw'] / f'run{run_number:06d}') / 'fu.lock'}"
     conversion = f"cmsRun cms_raw_evt_building.py rawDirectory={directories['raw']} convertedDirectory={directories['converted']} runNumber={run_number}"
 
