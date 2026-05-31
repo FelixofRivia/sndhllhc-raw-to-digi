8fd152a859d2ae091f8bbcefe2a58db5dad30f69
FelixofRivia <filippomei99@gmail.com>
2026-05-31 23:59:35 +0200

    folder

--- raw_to_digi/src/real_time_monitoring.cxx
+++ raw_to_digi/src/real_time_monitoring.cxx
@@ -11,6 +11,7 @@
 #include "TCanvas.h"
 #include "TH1D.h"
 #include "TFile.h"
+#include "TDirectory.h"
 
 #if USE_ROOT7_RHIST
 #include "ROOT/RHist.hxx"
@@ -94,8 +95,11 @@ int main(int argc, char* argv[]){
         layer_map[d.layer].insert({d.row, d.column});
     }
 
-    std::vector<ROOT::RDF::RResultPtr<TH1D>> histos_1d;
-    std::vector<ROOT::RDF::RResultPtr<TH2D>> histos_2d;
+    std::vector<ROOT::RDF::RResultPtr<TH1D>> histos_1d_global;
+    std::vector<ROOT::RDF::RResultPtr<TH2D>> histos_2d_global;
+
+    std::map<int, std::vector<ROOT::RDF::RResultPtr<TH1D>>> histos_1d;
+    std::map<int, std::vector<ROOT::RDF::RResultPtr<TH2D>>> histos_2d;
     std::vector<ROOT::RDF::RResultPtr<TProfile>> profiles;
 
     const int max_layer = layer_map.rbegin()->first;
@@ -127,16 +131,16 @@ int main(int argc, char* argv[]){
         }, {"adc", "layer"});
 
 
-    histos_1d.emplace_back(df3.Histo1D<ROOT::RVec<int>>({"layer", "layer;layer;Entries", max_layer + 1, 0, static_cast<double>(max_layer + 1)}, "layer"));
-    histos_1d.emplace_back(df3.Histo1D<ROOT::RVec<uint32_t>>({"det_id (advsndsw)", "det_id (advsndsw);det_id;Entries", 18000, 0, 180000}, "detector_id"));
-    histos_2d.emplace_back(df4.Histo2D<ROOT::RVec<int>, ROOT::RVec<float>>({"saturated percentage (adc > 253) vs layer", "saturated percentage (adc > 253) vs layer;layer;saturated %", max_layer + 1, 0, static_cast<double>(max_layer + 1), 101, 0, 101}, "layer_vec", "saturated_percentage_vec"));
-    histos_2d.emplace_back(df4.Histo2D<ROOT::RVec<int>, ROOT::RVec<uint32_t>>({"adc sum vs layer", "adc sum vs layer;layer;adc sum", max_layer + 1, 0, static_cast<double>(max_layer + 1), 1000, 0, 30000}, "layer_vec", "adc_vec"));
+    histos_1d_global.emplace_back(df3.Histo1D<ROOT::RVec<int>>({"layer", "layer;layer;Entries", max_layer + 1, 0, static_cast<double>(max_layer + 1)}, "layer"));
+    histos_1d_global.emplace_back(df3.Histo1D<ROOT::RVec<uint32_t>>({"det_id (advsndsw)", "det_id (advsndsw);det_id;Entries", 18000, 0, 180000}, "detector_id"));
+    histos_2d_global.emplace_back(df4.Histo2D<ROOT::RVec<int>, ROOT::RVec<float>>({"saturated percentage (adc > 253) vs layer", "saturated percentage (adc > 253) vs layer;layer;saturated %", max_layer + 1, 0, static_cast<double>(max_layer + 1), 101, 0, 101}, "layer_vec", "saturated_percentage_vec"));
+    histos_2d_global.emplace_back(df4.Histo2D<ROOT::RVec<int>, ROOT::RVec<uint32_t>>({"adc sum vs layer", "adc sum vs layer;layer;adc sum", max_layer + 1, 0, static_cast<double>(max_layer + 1), 1000, 0, 30000}, "layer_vec", "adc_vec"));
 
-    histos_1d.emplace_back(df3.Histo1D<ROOT::RVec<double>>({"x", "x;x [cm];Entries", 1000, x_min, x_max}, "x_vertical"));
-    histos_1d.emplace_back(df3.Histo1D<ROOT::RVec<double>>({"y", "y;y [cm];Entries", 1000, y_min, y_max}, "y_not_vertical"));
-    histos_1d.emplace_back(df3.Histo1D<ROOT::RVec<double>>({"z", "z;z [cm];Entries", 1000, z_min, z_max}, "z"));
-    histos_2d.emplace_back(df4.Histo2D<ROOT::RVec<double>, ROOT::RVec<double>>({"x vs z", "x vs z;z [cm];x [cm]", 1000, z_min, z_max, 1000, x_min, x_max}, "z_vertical", "x_vertical"));
-    histos_2d.emplace_back(df4.Histo2D<ROOT::RVec<double>, ROOT::RVec<double>>({"y vs z", "y vs z;z [cm];y [cm]", 1000, z_min, z_max, 1000, y_min, y_max}, "z_not_vertical", "y_not_vertical"));
+    histos_1d_global.emplace_back(df3.Histo1D<ROOT::RVec<double>>({"x", "x;x [cm];Entries", 1000, x_min, x_max}, "x_vertical"));
+    histos_1d_global.emplace_back(df3.Histo1D<ROOT::RVec<double>>({"y", "y;y [cm];Entries", 1000, y_min, y_max}, "y_not_vertical"));
+    histos_1d_global.emplace_back(df3.Histo1D<ROOT::RVec<double>>({"z", "z;z [cm];Entries", 1000, z_min, z_max}, "z"));
+    histos_2d_global.emplace_back(df4.Histo2D<ROOT::RVec<double>, ROOT::RVec<double>>({"x vs z", "x vs z;z [cm];x [cm]", 1000, z_min, z_max, 1000, x_min, x_max}, "z_vertical", "x_vertical"));
+    histos_2d_global.emplace_back(df4.Histo2D<ROOT::RVec<double>, ROOT::RVec<double>>({"y vs z", "y vs z;z [cm];y [cm]", 1000, z_min, z_max, 1000, y_min, y_max}, "z_not_vertical", "y_not_vertical"));
 
     ROOT::RDF::TProfile1DModel saturation_model(
         "saturated percentage (adc > 253) vs layer (profiled)",
@@ -167,14 +171,14 @@ int main(int argc, char* argv[]){
 
         if (layer % 2 == 0) {
             auto df_layer = df3.Define("row_layer", select_row).Define("column_layer", select_column).Define("x_vertical_layer", select_x);
-            histos_1d.emplace_back(df_layer.Histo1D<ROOT::RVec<double>>({h_x_name.c_str(), (h_x_name + std::string(";x [cm];Entries")).c_str(), 1000, x_min, x_max}, "x_vertical_layer"));
-            histos_2d.emplace_back(df_layer.Histo2D<ROOT::RVec<int>, ROOT::RVec<int>>({h_row_vs_column_name.c_str(), (h_row_vs_column_name + std::string(";column;row;Entries")).c_str(), max_column + 1, 0, static_cast<double>(max_column + 1), max_row + 1, 0, static_cast<double>(max_row + 1)}, "column_layer", "row_layer"));
+            histos_1d[layer].emplace_back(df_layer.Histo1D<ROOT::RVec<double>>({h_x_name.c_str(), (h_x_name + std::string(";x [cm];Entries")).c_str(), 1000, x_min, x_max}, "x_vertical_layer"));
+            histos_2d[layer].emplace_back(df_layer.Histo2D<ROOT::RVec<int>, ROOT::RVec<int>>({h_row_vs_column_name.c_str(), (h_row_vs_column_name + std::string(";column;row;Entries")).c_str(), max_column + 1, 0, static_cast<double>(max_column + 1), max_row + 1, 0, static_cast<double>(max_row + 1)}, "column_layer", "row_layer"));
 
         }
         else {
             auto df_layer = df3.Define("row_layer", select_row).Define("column_layer", select_column).Define("y_not_vertical_layer", select_y);
-            histos_1d.emplace_back(df_layer.Histo1D<ROOT::RVec<double>>({h_y_name.c_str(), (h_y_name + std::string(";y [cm];Entries")).c_str(), 1000, y_min, y_max}, "y_not_vertical_layer"));
-            histos_2d.emplace_back(df_layer.Histo2D<ROOT::RVec<int>, ROOT::RVec<int>>({h_row_vs_column_name.c_str(), (h_row_vs_column_name + std::string(";column;row;Entries")).c_str(), max_column + 1, 0, static_cast<double>(max_column + 1), max_row + 1, 0, static_cast<double>(max_row + 1)}, "column_layer", "row_layer"));
+            histos_1d[layer].emplace_back(df_layer.Histo1D<ROOT::RVec<double>>({h_y_name.c_str(), (h_y_name + std::string(";y [cm];Entries")).c_str(), 1000, y_min, y_max}, "y_not_vertical_layer"));
+            histos_2d[layer].emplace_back(df_layer.Histo2D<ROOT::RVec<int>, ROOT::RVec<int>>({h_row_vs_column_name.c_str(), (h_row_vs_column_name + std::string(";column;row;Entries")).c_str(), max_column + 1, 0, static_cast<double>(max_column + 1), max_row + 1, 0, static_cast<double>(max_row + 1)}, "column_layer", "row_layer"));
         }
        
         for (const auto& [row, col] : modules) {
@@ -196,30 +200,54 @@ int main(int argc, char* argv[]){
             auto df_module = df3.Define("adc_module", select_adc).Define("sistrip_module", select_strip).Define("nhits_module", "adc_module.size()").Define("saturated_percentage_module", compute_saturated_percentage, {"adc_module"})
                 .Define("cluster_adc_module", select_cluster_adc).Define("cluster_size_module", select_cluster_size);
 
-            histos_1d.emplace_back(df_module.Histo1D<ROOT::RVec<uint16_t>>({h_adc_name.c_str(), (h_adc_name + std::string(";adc;Entries")).c_str(), 256, 0, 256}, "adc_module"));
-            histos_1d.emplace_back(df_module.Histo1D<ROOT::RVec<uint16_t>>({h_strip_name.c_str(), (h_strip_name + std::string(";strip;Entries")).c_str(), MAX_SISTRIPS_PER_MODULE, 0, static_cast<double>(MAX_SISTRIPS_PER_MODULE)}, "sistrip_module"));
-            histos_1d.emplace_back(df_module.Histo1D<std::size_t>({h_nhits_name.c_str(), (h_nhits_name + std::string(";nhits;Entries")).c_str(), MAX_SISTRIPS_PER_MODULE, 0, static_cast<double>(MAX_SISTRIPS_PER_MODULE)}, "nhits_module"));
-            histos_1d.emplace_back(df_module.Histo1D<ROOT::RVec<uint32_t>>({h_cluster_adc_name.c_str(), (h_cluster_adc_name + std::string(";adc;Entries")).c_str(), 3000, 0, 3000}, "cluster_adc_module"));
-            histos_1d.emplace_back(df_module.Histo1D<ROOT::RVec<size_t>>({h_cluster_size_name.c_str(), (h_cluster_size_name + std::string(";size;Entries")).c_str(), 100, 0, 100}, "cluster_size_module"));
-            histos_1d.emplace_back(df_module.Histo1D<float>({h_saturated_percentage_name.c_str(), (h_saturated_percentage_name + std::string(";saturated %;Entries")).c_str(), 101, 0, 101}, "saturated_percentage_module"));
-            histos_2d.emplace_back(df_module.Histo2D<ROOT::RVec<uint16_t>, ROOT::RVec<uint16_t>>({h_adc_vs_strip_name.c_str(), (h_adc_vs_strip_name + std::string(";strip;adc;Entries")).c_str(),  MAX_SISTRIPS_PER_MODULE, 0, static_cast<double>(MAX_SISTRIPS_PER_MODULE), 256, 0, 256}, "sistrip_module", "adc_module"));
-            histos_2d.emplace_back(df_module.Histo2D<ROOT::RVec<size_t>, ROOT::RVec<uint32_t>>({h_cluster_adc_vs_size_name.c_str(), (h_cluster_adc_vs_size_name + std::string(";size;adc;Entries")).c_str(), 100, 0, 100, 3000, 0, 3000}, "cluster_size_module", "cluster_adc_module"));
+            histos_1d[layer].emplace_back(df_module.Histo1D<ROOT::RVec<uint16_t>>({h_adc_name.c_str(), (h_adc_name + std::string(";adc;Entries")).c_str(), 256, 0, 256}, "adc_module"));
+            histos_1d[layer].emplace_back(df_module.Histo1D<ROOT::RVec<uint16_t>>({h_strip_name.c_str(), (h_strip_name + std::string(";strip;Entries")).c_str(), MAX_SISTRIPS_PER_MODULE, 0, static_cast<double>(MAX_SISTRIPS_PER_MODULE)}, "sistrip_module"));
+            histos_1d[layer].emplace_back(df_module.Histo1D<std::size_t>({h_nhits_name.c_str(), (h_nhits_name + std::string(";nhits;Entries")).c_str(), MAX_SISTRIPS_PER_MODULE, 0, static_cast<double>(MAX_SISTRIPS_PER_MODULE)}, "nhits_module"));
+            histos_1d[layer].emplace_back(df_module.Histo1D<ROOT::RVec<uint32_t>>({h_cluster_adc_name.c_str(), (h_cluster_adc_name + std::string(";adc;Entries")).c_str(), 3000, 0, 3000}, "cluster_adc_module"));
+            histos_1d[layer].emplace_back(df_module.Histo1D<ROOT::RVec<size_t>>({h_cluster_size_name.c_str(), (h_cluster_size_name + std::string(";size;Entries")).c_str(), 100, 0, 100}, "cluster_size_module"));
+            histos_1d[layer].emplace_back(df_module.Histo1D<float>({h_saturated_percentage_name.c_str(), (h_saturated_percentage_name + std::string(";saturated %;Entries")).c_str(), 101, 0, 101}, "saturated_percentage_module"));
+            histos_2d[layer].emplace_back(df_module.Histo2D<ROOT::RVec<uint16_t>, ROOT::RVec<uint16_t>>({h_adc_vs_strip_name.c_str(), (h_adc_vs_strip_name + std::string(";strip;adc;Entries")).c_str(),  MAX_SISTRIPS_PER_MODULE, 0, static_cast<double>(MAX_SISTRIPS_PER_MODULE), 256, 0, 256}, "sistrip_module", "adc_module"));
+            histos_2d[layer].emplace_back(df_module.Histo2D<ROOT::RVec<size_t>, ROOT::RVec<uint32_t>>({h_cluster_adc_vs_size_name.c_str(), (h_cluster_adc_vs_size_name + std::string(";size;adc;Entries")).c_str(), 100, 0, 100, 3000, 0, 3000}, "cluster_size_module", "cluster_adc_module"));
         }
     }
 
+    // Write histos
     TFile output_file(output_root_file.c_str(), "RECREATE");
 
-    for (auto& h : histos_1d) {
+    for (auto& h : histos_1d_global) {
         h->SetFillColor(kAzure - 9);
         h->Write();
     }
 
-    for (auto& h : histos_2d) {
+    for (auto& h : histos_2d_global) {
         h->Write();
     }
 
     for (auto& p : profiles) {
         p->Write();
+    }
+
+    for (auto& [layer, hlist] : histos_1d) {
+
+        std::string dir_name = "layer_" + std::to_string(layer);
+        TDirectory* dir = output_file.mkdir(dir_name.c_str());
+        if (!dir) dir = output_file.GetDirectory(dir_name.c_str());
+
+        for (auto& h : hlist) {
+            h->SetFillColor(kAzure - 9);
+            dir->WriteTObject(h.GetPtr());
+        }
+    }
+
+    for (auto& [layer, hlist] : histos_2d) {
+
+        std::string dir_name = "layer_" + std::to_string(layer);
+        TDirectory* dir = output_file.GetDirectory(dir_name.c_str());
+        if (!dir) dir = output_file.mkdir(dir_name.c_str());
+
+        for (auto& h : hlist) {
+            dir->WriteTObject(h.GetPtr());
+        }
     }
 
     output_file.Close();
